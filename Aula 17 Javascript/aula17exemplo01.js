027b21005650e482d5020e411f6c68b758d9835c
Francieli-Cordeiro <152501477+Francieli-Cordeiro@users.noreply.github.com>
2023-11-30 18:21:28 -0300

    1 arquivo

---
+++ Aula 17 Javascript/aula17exemplo01.js
@@ -0,0 +1,26 @@
+let frase = "alo alunos da tuma 7"
+console.log(frase)
+let numero = 1
+console.log (numero)
+let numeroDecimal = 1.5
+console.log(numeroDecimal)
+let soma = 2+2
+console.log(soma)
+let subtração = 10-5
+console.log(subtração)
+let multiplicação= 2*5
+console.log(multiplicação)
+let divisão = 10/2
+console.log(divisão)
+let potenciação = 5**2
+console.log(potenciação)
+let resto = 9%2
+console.log(resto)
+let soma3 = 3+8+9
+console.log(soma3)
+
+
+
+
+
+
